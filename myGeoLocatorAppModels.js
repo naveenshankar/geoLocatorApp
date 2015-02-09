e44ebf0e9434ab23d2db2c5e1c9f92c64177c38b
naveenshankar <naveenshankarcse@gmail.com>
2015-02-09 07:14:37 -0800

    Update myGeoLocatorAppModels.js

--- myGeoLocatorAppModels.js
+++ myGeoLocatorAppModels.js
@@ -1,26 +1,18 @@
 var myGeoLocatorAppModels = function(){
-	this.formData = Backbone.Model.extend(
+	this.mapData = Backbone.Model.extend(
 		    {
 		        defaults : {
-		        			"userName":"Enter your username",
-		        			"password":'Enter your password',
-		        			"currentAnimationRequestId":'',
-		        			"reloadFlag" : false,
-		        			"withAnimation" : false,
-		        			"withoutAnimation" : false,
-		        			"mockDataTables" : {ip:"173.194.116.98",country_code:"US",country_name:"United States",region_code:"CA",region_name:"California",city:"Mountain View",zip_code:"94043",time_zone:"America/Los_Angeles",latitude:37.419,longitude:-122.058,metro_code:807},
-		        			"dataTables" : ''
+		        			"myCompanyDescriptor" : '',
+		        			"myErrorMsgs":{syntax:"Enter a valid email address. Eg. 'www.nytimes.com', 'nytimes.com' or 'g1.com.br' or 'www.g1.com.br'",unavailable:"Website not Found!"},
+		        			"errorMsgFlag":false,
+		        			"currentErrorMsg": "",
+		        			"myLocationMockData" : {ip:"173.194.116.98",country_code:"US",country_name:"United States",region_code:"CA",region_name:"California",city:"Mountain View",zip_code:"94043",time_zone:"America/Los_Angeles",latitude:37.419,longitude:-122.058,metro_code:807},
+		        			"myLocationData" : null,
+		        			"myCompanyLocationMockData" : {ip:"173.194.116.98",country_code:"US",country_name:"United States",region_code:"CA",region_name:"California",city:"Mountain View",zip_code:"94043",time_zone:"America/Los_Angeles",latitude:37.419,longitude:-122.058,metro_code:807},
+		        			"myCompanyLocationData" : null
 		        			}
 //{"ip":"173.194.116.98","country_code":"US","country_name":"United States","region_code":"CA","region_name":"California","city":"Mountain View","zip_code":"94043","time_zone":"America/Los_Angeles","latitude":37.419,"longitude":-122.058,"metro_code":807}
 
 		    });
 
-	this.mapData = Backbone.Model.extend(
-		    {
-		    	 defaults : {
-		        			"userName":"Enter your username",
-		        			"password":'Enter your password',
-		        			"currentPage":''	 
-		        			}
-		    });
 }
