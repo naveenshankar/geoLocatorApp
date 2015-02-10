3575bf4734eb285a05b0dbc2786a95d99936b1ca
naveenshankar <naveenshankarcse@gmail.com>
2015-02-09 20:41:58 -0800

    Update myGeoLocatorAppUnitTest.js

--- myGeoLocatorAppUnitTest.js
+++ myGeoLocatorAppUnitTest.js
@@ -1,17 +1,18 @@
 $( document ).ready(function() {
-    /*var newApp = new myJSONAppMain();
-    console.log(newApp);
-    var utils = newApp.utils;
+    var newApp = new myGeoLocatorAppMain();
+    
+	    QUnit.test( "errormessagesfadeIn", function(assert) {
+	    		newApp.formView.model.set('errorMsgFlag',true);
+	    		setTimeout(assert.equal( $('.errormsgs').css('display'), "block", "Error messages fade-in works!" ),2000);
+	    		newApp.formView.model.set('errorMsgFlag',false);
+		});
 
-	    test( "ChartDataFormat", function() {
-
-			  var newObject = {};
- 			  newObject= newApp.myRecordsView.model.get('mockDataRecords');
-			  var resultData = utils.formatDataforCharts(newObject,[],[],[]);
-			  
-			  notEqual( resultData[0].length, 0, "Number of flowers should not be 0" );
-			  notEqual( resultData[1].length, 0, "Number of flowers Sold should not be 0" );
-			  notEqual( resultData[2].length, 0, "Number of flowers unSold should not be 0" );
-			  
-		});*/
+		QUnit.test( "currentErrorTexts", function(assert) {
+	    		newApp.formView.model.set('currentErrorMsg',"syntax");
+	    		assert.equal($('.errorText').html(), "Enter a valid email address. Eg. 'www.nytimes.com', 'nytimes.com' or 'g1.com.br' or 'www.g1.com.br'", "Dynamic Error messages work!" );
+	    		newApp.formView.model.set('currentErrorMsg',"unavailable");
+	    		assert.equal($('.errorText').html(), "Website not Found!", "Dynamic Error messages work!" );
+	    		newApp.formView.model.set('currentErrorMsg',"");
+		});
+	
 });
