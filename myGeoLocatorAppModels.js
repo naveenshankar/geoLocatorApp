c507b60522d06ff6b0ce798d9f71431d36a4f60a
naveenshankar <naveenshankarcse@gmail.com>
2015-02-17 21:48:49 -0800

    Update myGeoLocatorAppModels.js

--- myGeoLocatorAppModels.js
+++ myGeoLocatorAppModels.js
@@ -6,6 +6,7 @@ var myGeoLocatorAppModels = function(){
 		        			"myErrorMsgs":{syntax:"Enter a valid email address. Eg. 'www.nytimes.com', 'nytimes.com' or 'g1.com.br' or 'www.g1.com.br'",unavailable:"Website not Found!"},
 		        			"errorMsgFlag":false,
 		        			"currentErrorMsg": "",
+		        			"newSearchText":"",
 		        			"myLocationMockData" : {ip:"173.194.116.98",country_code:"US",country_name:"United States",region_code:"CA",region_name:"California",city:"Mountain View",zip_code:"94043",time_zone:"America/Los_Angeles",latitude:37.419,longitude:-122.058,metro_code:807},
 		        			"myLocationData" : null,
 		        			"myCompanyLocationMockData" : {ip:"173.194.116.98",country_code:"US",country_name:"United States",region_code:"CA",region_name:"California",city:"Mountain View",zip_code:"94043",time_zone:"America/Los_Angeles",latitude:37.419,longitude:-122.058,metro_code:807},
