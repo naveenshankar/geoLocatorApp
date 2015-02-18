2370bd8decb3f43fec39443dc0225817f85ff6b9
naveenshankar <naveenshankarcse@gmail.com>
2015-02-17 21:46:32 -0800

    Update myGeoLocatorAppMain.js

--- myGeoLocatorAppMain.js
+++ myGeoLocatorAppMain.js
@@ -8,6 +8,7 @@ var myGeoLocatorAppMain = function()
 	this.formView = new views.formView({el:'.mainForm',model:this.mapData});
 	this.formView.listenTo(this.mapData, 'change:errorMsgFlag', this.formView.showHideErrorMsg);
 	this.formView.listenTo(this.mapData, 'change:currentErrorMsg', this.formView.changeErrorMessage);
+	//this.formView.listenTo(this.mapData, 'change:newSearchText', this.formView.updateLocalStorage);
 
 	this.mapView = new views.mapView({el:'.mapLocation',model:this.mapData});
 	this.mapView.listenTo(this.mapData, 'change:myCompanyLocationData', this.mapView.renderMyCompanyLocation);
